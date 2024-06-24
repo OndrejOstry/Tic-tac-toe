8c1c19b33ebb72665caab0d84fa4d820b0874a69
OndrejOstry <ondrejostry@gmail.com>
2024-06-24 19:59:17 +0200

    added Greetings

--- script.py
+++ script.py
@@ -1,1 +1,31 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+author: Ondřej Ostrý
+email: ondrejostry@gmail.com
+discord: ondrejostry
+"""
+
+
+
+
+
+
+
+
+def greet():
+    print(f"""Welcome to Tic Tac Toe
+{"=" * 40}
+GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+ * horizontal
+ * vertical or
+ * diagonal row
+{"=" * 40}
+Let's start the game""")
+
+greet()
+
 
